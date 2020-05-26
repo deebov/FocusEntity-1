41c1a7da5a3845f807259075bf48214f69a10612
Dilshod Turobov <deebov@yandex.com>
2020-05-26 19:37:41 +0500

    replace hittest with raycastgst

--- Package.swift
+++ Package.swift
@@ -9,13 +9,7 @@ let package = Package(
   products: [
     .library(name: "FocusEntity", targets: ["FocusEntity"])
   ],
-  dependencies: [
-//  .package(path: "../ARKit-SmartHitTest")
-    .package(
-      url: "https://github.com/maxxfrazer/ARKit-SmartHitTest",
-      .upToNextMajor(from: "2.0.0")
-    )
-  ],
+  dependencies: [],
   targets: [
     .target(name: "FocusEntity", dependencies: ["SmartHitTest"])
   ],
